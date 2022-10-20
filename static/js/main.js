08393757e517c192cd5b6b8230275e35bbb6b346
mayuresh wankhede <mayureshwankhede21@gmail.com>
2022-10-19 22:27:03 -0400

    minor changes to book a demo

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,33 @@
-$('.nav-label').on('click', function () {
-  $('nav').toggleClass('menu-active');
-  $(this).toggleClass('nav-label-open');
+$(".nav-label").on("click", function () {
+  $("nav").toggleClass("menu-active");
+  $(this).toggleClass("nav-label-open");
 });
 
 // jQuery.
-$(function() {
-    // Reference the tab links.
-    const tabLinks = $('#tab-links li a');
-    
-    // Handle link clicks.
-    tabLinks.click(function(event) {
-        var $this = $(this);
-        
-        // Prevent default click behaviour.
-        event.preventDefault();
-        
-        // Remove the active class from the active link and section.
-        $('#tab-links a.active, section.active').removeClass('active');
-        
-        // Add the active class to the current link and corresponding section.
-        $this.addClass('active');
-        $($this.attr('href')).addClass('active');
-    });
+$(function () {
+  // Reference the tab links.
+  const tabLinks = $("#tab-links li a");
+
+  // Handle link clicks.
+  tabLinks.click(function (event) {
+    var $this = $(this);
+
+    // Prevent default click behaviour.
+    event.preventDefault();
+
+    // Remove the active class from the active link and section.
+    $("#tab-links a.active, section.active").removeClass("active");
+
+    // Add the active class to the current link and corresponding section.
+    $this.addClass("active");
+    $($this.attr("href")).addClass("active");
+  });
 });
 
-$(document).ready(function() {
-    $('a#contact-popup').magnificPopup({
-      items: {
-          src: `<section class="wrapper push-2 contact-form">
+$(document).ready(function () {
+  $("a#contact-popup").magnificPopup({
+    items: {
+      src: `<section class="wrapper push-2 contact-form">
                   <div class="grid pop-up-padding">
                     <div class="unit s-1 m-1-3">
                       <a class="logo logo-contact" href="/"><img class="img-flex logo" src="/images/pomme-logo.png" alt="POMMe-Health coloured logo"></a>
@@ -43,7 +43,7 @@ $(document).ready(function() {
                       </a></p>
                     </div>
                     <div class="unit s-1 m-2-3">
-                      <form action="https://formspree.io/f/mjvldwov" method="POST" id="contact-popup" class="wrapper request-form white-popup">
+                      <form action="https://formspree.io/f/meqdznkk" method="POST" id="contact-popup" class="wrapper request-form white-popup">
                         <h4 class="text-center">Request a Demo</h4>
                         <input type="text" class="form-spacing" id="fullName" name="fullName" required placeholder="Full Name">
                         <input type="email" class="form-spacing" id="email" name="email" required placeholder="E-mail">
@@ -54,7 +54,7 @@ $(document).ready(function() {
                     </div>
                   </div>
                 </section>`,
-          type: 'inline'
-      }
-    });
+      type: "inline",
+    },
+  });
 });
