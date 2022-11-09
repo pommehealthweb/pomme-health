06c66a28dc7901229aeae3e8815d68a0989bd52d
mayuresh wankhede <mayureshwankhede21@gmail.com>
2022-11-08 19:38:32 -0500

    card 58 completed

--- static/js/main.js
+++ static/js/main.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
                       </a></p>
                     </div>
                     <div class="unit s-1 m-2-3">
-                      <form action="https://formspree.io/f/meqdznkk" method="POST" id="contact-popup" class="wrapper request-form white-popup">
+                      <form action="https://formspree.io/f/moqbywaw" method="POST" id="contact-popup" class="wrapper request-form white-popup">
                         <h4 class="text-center">Request a Demo</h4>
                         <input type="text" class="form-spacing" id="fullName" name="fullName" required placeholder="Full Name">
                         <input type="email" class="form-spacing" id="email" name="email" required placeholder="E-mail">
