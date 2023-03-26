ac8ab7d9e6a51cdc8a2e29f9b5af640c18bcd454
Kevin Garry <kevingarry97@gmail.com>
2023-03-26 02:36:45 -0700

    Fixing Modals Alignments & Function Outcomes

--- static/js/main.js
+++ static/js/main.js
@@ -29,26 +29,33 @@ $(document).ready(function () {
     items: {
       src: `<section class="wrapper push-2 contact-form">
                   <a href="#" class="closeButton" id="close-btn"><i class="fa fa-times" aria-hidden="true"></i></a>
-                  <div class="grid pop-up-padding">
-                    <div class="unit s-1 m-1-3">
-                      <a class="logo logo-contact" href="/"><img class="img-flex logo" src="/images/pomme-logo.png" alt="POMMe-Health coloured logo"></a>
-                      <p>POMMe-Health delivers a <br> communication
+                  <div class="grid pop-up-padding justify-even">
+                    <div class="unit s-1 m-1-3 pt-1">
+                      <div class="text-center">
+                        <a class="logo logo-contact" href="/"><img class="img-flex logo" src="/images/pomme-logo.png" alt="POMMe-Health coloured logo"></a>
+                      </div>
+                      <p class="text-center">POMMe-Health delivers a <br> communication
                       platform that<br>
                       eliminates gaps in healthcare.</p>
-                      <h3 class="request-demo">Request a Demo Today!</h3>
-                      <p class="email-us">Email us:</p>
-                      <a class="email-us" href="mailto:info@pomme-health.com">info@pomme-health.com</a>
-                      <p class="linked-in-contact"><a target="_blank" href="https://www.linkedin.com/company/pomme-health/">
-                        <i class="icon i-32"><img src="/images/linkedin.svg"></i>
-                      </a></p>
+                      <div class="mt-4">
+                        <p class="text-center">Form more information please <br> email us:</p>
+                        <div class="text-center">
+                          <a class="email-us" href="mailto:info@pomme-health.com"><small class="text-center">info@pomme-health.com</small></a>
+                        </div>
+                        <div class="text-center pt-1">
+                          <a target="_blank" href="https://www.linkedin.com/company/pomme-health/">
+                            <i class="icon i-32"><img src="/images/linkedin.svg"></i>
+                          </a>
+                        </div>
+                      </div>
                     </div>
                     <div class="unit s-1 m-2-3">
                       <form action="https://formsubmit.co/demo@pomme-health.com" method="POST" id="contact-popup" class="wrapper request-form white-popup">
-                        <h4 class="text-center">Request a Demo</h4>
-                        <input type="text" class="form-spacing" id="fullName" name="name" required placeholder="Full Name">
-                        <input type="email" class="form-spacing" id="email" name="email" required placeholder="E-mail">
-                        <input type="text" class="form-spacing" id="subject" name="subject" value="Request a Demo" placeholder="Request a Demo" disabled>
-                        <textarea id="message" class="form-spacing" name="message" rows="3" required placeholder="Message"></textarea>
+                        <h3 class="request-demo">Request a Demo Today!</h3>
+                        <input type="text" class="form-spacing f-18" id="fullName" name="name" required placeholder="Full Name">
+                        <input type="email" class="form-spacing f-18" id="email" name="email" required placeholder="Email">
+                        <input type="text" class="form-spacing f-18" id="subject" name="subject" value="Request a Demo" placeholder="Request a Demo" disabled>
+                        <textarea id="message" class="form-spacing f-18" name="message" rows="3" required placeholder="Message"></textarea>
                         <button type="submit" class="nav-btn center">Send</button>
                       </form>
                     </div>
@@ -134,7 +141,7 @@ $(document).ready(function () {
                 <h4 class="text-center">Join a Focus Group</h4>
                 <input type="text" class="form-spacing" id="fullName" name="name" required placeholder="Full Name">
                 <input type="email" class="form-spacing" id="email" name="email" required placeholder="E-mail">
-                <input type="text" class="form-spacing" id="subject" name="subject" value="Start a Pilot Project" placeholder="Join a Focus Group" disabled>
+                <input type="text" class="form-spacing" id="subject" name="subject" value="Join a Focus Group" placeholder="Join a Focus Group" disabled>
                 <textarea id="message" class="form-spacing" name="message" rows="3" required placeholder="Message"></textarea>
                 <button type="submit" class="nav-btn center">Send</button>
               </form>
